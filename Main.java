082800124785fbb4f1b90d04dcf89645ac319350
ZotovM <90614959+ZotovM@users.noreply.github.com>
2021-10-27 13:17:27 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,42 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите высоту фигуры: ");
-        int h = scanner.nextInt();
+        int h = readNum();
+        checkNum(h);
 
-        if (h < 1) {
-            System.out.println("Ошибка! Введите число больше единицы. ");
-            System.exit(1);
-        }
-        
         System.out.print("Введите ширину фигуры: ");
-        int w = scanner.nextInt();
-
-        if (w < 1) {
-            System.out.println("Ошибка! Введите число больше единицы. ");
-            System.exit(1);
-        }
+        int w = readNum();
+        checkNum(w);
 
         printFigure(h, w);
 
     }
 
-    public static void printFigure( int h, int w) {
-        for(int i = 0; i < h; i++) {
-            for(int j = 0; j < w; j++){
-                if(i == 0 || j == 0) {
-                    System.out.print("a");
-                }
-                else {
-                    System.out.print("b");
-                }
+    private static int readNum() {
+        Scanner scn = new Scanner(System.in);
+        return scn.nextInt();
+    }
+
+    private static void checkNum(int x) {
+        if (x < 1) {
+            System.out.println("Ошибка! Введите число больше единицы. ");
+            System.exit(1);
+        }
+    }
+
+    public static void printFigure(int h, int w) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 1; j <= w; j++) {
+                System.out.print("ab");
+
             }
+
             System.out.println();
         }
         System.exit(0);
-
     }
+
 }
